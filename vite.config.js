d994bc10f94a2dda92b9d0841f8ee9163486d376
Xeyn19 <edgarrodilorosa@gmail.com>
2025-04-05 14:40:40 +0800

    fix last

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,7 @@ export default defineConfig({
   server: {
     port:3000,
   },
-  base: process.env.VITE_BASE_PATH || "/coffee-shop",
+  base: mode === 'production'
+  ? process.env.VITE_BASE_PATH || '/coffee-shop'
+  : '/', 
 })
