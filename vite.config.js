4ddb43beb4e9c3c83bb77a14045cac4015d96533
Xeyn19 <edgarrodilorosa@gmail.com>
2025-04-05 14:42:04 +0800

    fix na tlga to

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port:3000,
-  },
-  base: mode === 'production'
-  ? process.env.VITE_BASE_PATH || '/coffee-shop'
-  : '/', 
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 3000,
+    },
+    base: mode === 'production'
+      ? process.env.VITE_BASE_PATH || '/coffee-shop' // specify base path for production
+      : '/', // base path for development
+  };
+});
