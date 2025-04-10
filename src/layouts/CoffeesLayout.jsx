9aeac2dbb506c8b990f54c9e6b85bffd7eda4344
Xeyn19 <edgarrodilorosa@gmail.com>
2025-04-10 14:34:57 +0800

    fix-last

--- src/layouts/CoffeesLayout.jsx
+++ src/layouts/CoffeesLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import coffees from '/about-coffees.jpg' // use correct path if needed
+
 
 const CoffeesLayout = () => {
   return (
